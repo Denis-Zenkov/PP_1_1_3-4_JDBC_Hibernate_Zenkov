3c8f65bf147146093c7e4693989be70cd4f32639
Denis-Zenkov <Denis-olegov@mail.ru>
2022-02-06 20:01:19 +0300

    Init

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -1,7 +1,24 @@
 package jm.task.core.jdbc;
 
+import jm.task.core.jdbc.model.User;
+import jm.task.core.jdbc.service.UserService;
+import jm.task.core.jdbc.service.UserServiceImpl;
+
+
 public class Main {
+    static UserService userService = new UserServiceImpl();
+    static User user1 = new User("Diego", "Diegovich", (byte) 34);
+    static User user2 = new User("Oksana", "Oksanovna", (byte) 24);
+    static User user3 = new User("Igor", "Leonidovich", (byte) 29);
+    static User user4 = new User("Ivan", "Ivanovich", (byte) 45);
     public static void main(String[] args) {
-        // реализуйте алгоритм здесь
+        userService.createUsersTable();
+        userService.saveUser(user1.getName(), user1.getLastName(), user1.getAge());
+        userService.saveUser(user2.getName(), user2.getLastName(), user2.getAge());
+        userService.saveUser(user3.getName(), user3.getLastName(), user3.getAge());
+        userService.saveUser(user4.getName(), user4.getLastName(), user4.getAge());
+        System.out.println(userService.getAllUsers());
+        userService.cleanUsersTable();
+        userService.dropUsersTable();
     }
 }
--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,5 +1,26 @@
 package jm.task.core.jdbc.util;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+
 public class Util {
     // реализуйте настройку соеденения с БД
+    private static String URL = "jdbc:mysql://localhost:3306/mydbtest";
+    private static String USERNAME = "root";
+    private static String PASSWORD = "MainAdminZenkov";
+
+    public static Connection getConnect(){
+        Connection connection = null;
+        try {
+            connection = DriverManager.getConnection(URL, USERNAME, PASSWORD);
+            Statement statement = connection.createStatement();
+            System.out.println("Соединение с БД установлено!");
+        } catch (SQLException e) {
+            System.out.println("Соединение с БД установить не удалось!");
+            e.printStackTrace();
+        }
+        return connection;
+    }
 }
